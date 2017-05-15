dc1c46863ff3797b6929f803687cc71bb6e686ee
Keven Marin <keven@delvelabs.ca>
2017-05-14 22:38:13 -0400

    Tweak button

--- src/components/SubmitFlagDialog.vue
+++ src/components/SubmitFlagDialog.vue
@@ -1,21 +1,8 @@
 <template type="text/html">
   <el-tooltip :disabled="!settings.board_read_only" class="item" effect="dark" content="You cannot submit flag at this time" placement="bottom">
-    <el-button type="text" class="submit-flag-button" @click="submitFlag" :disabled="settings.board_read_only">Submit Flag</el-button>
+    <el-button type="primary" @click="submitFlag" :disabled="settings.board_read_only">Submit Flag</el-button>
   </el-tooltip>
 </template>
-
-<style scoped>
-.submit-flag-button,
-.submit-flag-button:hover,
-.submit-flag-button:active {
-  background-color: transparent;
-  color: #fff;
-  opacity: 1;
-}
-.submit-flag-button {
-  opacity: .8;
-}
-</style>
 
 <script>
 export default {
