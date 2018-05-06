17f2dc3b64c3df263818059fa7a350fc4dc3f3e8
Keven Marin <keven@delvelabs.ca>
2018-05-06 09:46:09 -0400

    Add deployment run scripts for github

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,15 @@
 const nodeExternals = require('webpack-node-externals')
 const resolve = (dir) => require('path').join(__dirname, dir)
 
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/askgod-webui/'
+  }
+} : {}
+
 module.exports = {
+  ...routerBase,
   /*
   ** Headers of the page
   */
