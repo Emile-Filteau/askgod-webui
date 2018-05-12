3410679dd1813ed2b1e82dcb2ecdfe7697b45343
Keven Marin <keven@delvelabs.ca>
2018-05-12 16:10:23 -0400

    Remove footer, right sidenav and mini-variant

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      :mini-variant.sync="app.miniVariant"
       :clipped="app.clipped"
       v-model="app.drawer"
       fixed
@@ -28,20 +27,7 @@
       app
       :clipped-left="app.clipped">
       <v-toolbar-side-icon @click="toggle('drawer')"/>
-      <v-btn
-        icon
-        @click.stop="toggle('miniVariant')"
-      >
-        <v-icon v-html="app.miniVariant ? 'chevron_right' : 'chevron_left'"/>
-      </v-btn>
       <v-toolbar-title v-text="title"/>
-      <v-spacer/>
-      <v-btn
-        icon
-        @click.stop="toggle('rightDrawer')"
-      >
-        <v-icon>settings</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container
@@ -50,28 +36,6 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="app.right"
-      v-model="app.rightDrawer"
-      fixed>
-      <v-list subheader>
-        <v-subheader>Settings</v-subheader>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-checkbox v-model="autoRefresh"/>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Auto Refresh</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="app.fixed"
-      app>
-      <span>NorthSec &copy; 2018</span>
-    </v-footer>
   </v-app>
 </template>
 
