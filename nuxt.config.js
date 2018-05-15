1884bf4b4a592cedcc5c3443b1b10fad78aa47cf
Keven Marin <keven@delvelabs.ca>
2018-05-14 22:18:41 -0400

    Update APP title

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Askgod - 2018',
+    title: 'NorthSec CTF - Askgod Web UI',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
