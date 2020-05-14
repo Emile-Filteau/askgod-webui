5d98f7422b0d7324bcc94aabdf29996ca46fd97d
Keven Marin <keven@delvesecurity.com>
2020-05-14 15:02:22 -0400

    test github action for unit test

--- store/mutations.test.js
+++ store/mutations.test.js
@@ -7,3 +7,26 @@ test('toggleSidebar', () => {
   mutations.toggleSidebar(state)
   expect(state.sidebar).toBe(true)
 })
+
+test('setFireworksDialog', () => {
+  const state = {
+    fireworksDialog: false,
+    latestScore: {
+      teamName: null,
+      score: null,
+    },
+  }
+  const data = {
+    show: true,
+    score: {
+      teamName: 'foo',
+      score: 2
+    }
+  }
+  mutations.setFireworksDialog(state, data)
+  expect(state.fireworksDialog).toBe(true)
+  expect(state.latestScore).toEqual({
+    teamName: 'foo',
+    score: 2,
+  })
+})
