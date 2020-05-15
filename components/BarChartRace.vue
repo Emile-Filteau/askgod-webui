05335c03716b062829c5c93a9f1654de2b0c54a6
Keven Marin <keven@delvesecurity.com>
2020-05-14 22:20:11 -0400

    Readme cleanup

---
+++ components/BarChartRace.vue
@@ -0,0 +1,31 @@
+<script>
+import { HorizontalBar, mixins } from 'vue-chartjs'
+
+export default {
+  extends: HorizontalBar,
+  mixins: [mixins.reactiveProp],
+  props: {
+    chartdata: {
+      type: Object,
+      default(input) {
+        return input
+      }
+    },
+    options: {
+      type: Object,
+      default() {
+        return {
+          maintainAspectRatio: false,
+          responsive: true,
+          legend: {
+            display: false
+          }
+        }
+      }
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options);
+  }
+}
+</script>
